f4d32638b4a783c2e6b7d07dd3103b078c1f1ee8
amindid <amine10ouchaadid@gmail.com>
2024-06-28 17:41:41 +0100

    day01 ex06

---
+++ srcs/day01/ex06/my_sort.py
@@ -0,0 +1,32 @@
+import sys
+
+def my_sort():
+    d = {
+        'Hendrix': '1942',
+        'Allman': '1946',
+        'King': '1925',
+        'Clapton': '1945',
+        'Johnson': '1911',
+        'Berry': '1926',
+        'Vaughan': '1954',
+        'Cooder': '1947',
+        'Page': '1944',
+        'Richards': '1943',
+        'Hammett': '1962',
+        'Cobain': '1967',
+        'Garcia': '1942',
+        'Beck': '1944',
+        'Santana': '1947',
+        'Ramone': '1948',
+        'White': '1975',
+        'Frusciante': '1970',
+        'Thompson': '1949',
+        'Burton': '1939',
+    }
+    d = dict(sorted(d.items()))
+    d = dict(sorted(d.items(), key=lambda item: item[1]))
+    for key,value in d.items():
+        print(key)
+
+if __name__ == "__main__":
+    my_sort()
