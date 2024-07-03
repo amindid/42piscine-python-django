b676464a1d201afb024f3f65f71838533a9d8cd3
amindid <amine10ouchaadid@gmail.com>
2024-07-03 23:58:48 +0100

    working on day02 ex00

---
+++ srcs/day02/ex00/render.py
@@ -0,0 +1,24 @@
+import sys, os, re
+
+def render():
+	if len(sys.argv) != 2:
+		print("arg error !!")
+		sys.exit(1)
+	try:
+		HTML = ""
+		pairs = []
+		with open(sys.argv[0], "r") as template:
+			HTML = template.read()
+		with open("./settings.py", "r") as settings:
+			for line in settings:
+				tmp = line.split("=")
+				pairs.append({tmp[0].strip(): tmp[1].strip().replace('"', '')})
+		with open("./result.html", "w") as result:
+			result.write(HTML.format(**pairs))
+	except Exception as e:
+		print(f"exception accured : {e}")
+		sys.exit(1)
+
+
+if __name__ == '__main__':
+	render()
