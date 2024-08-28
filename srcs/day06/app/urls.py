c40723c73d5ae5cb6a2b00ac51733144632885bb
amindid <amine10ouchaadid@gmail.com>
2024-08-28 17:39:25 +0100

    working on day06 ex01

--- srcs/day06/app/urls.py
+++ srcs/day06/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.homepage, name='homepage'),
-	# path('get_name', views.get_name, name='get_name')
+	path('login', views.login, name='login'),
+	path('registration', views.registration, name='registration')
 ]
--- srcs/day06/app/views.py
+++ srcs/day06/app/views.py
@@ -1,9 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import authenticate, login as auth_login
 import time
 import random
 from django.conf import settings
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		try :
+			user = User.objects.get(username=username)
+			password = request.POST.get('password')
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				auth_login(request,user)
+				request.session['username'] = username
+				request.session['creation_time'] = time.time()
+				return redirect('homepage')
+			else:
+				messages.error(request, 'incorrect password !!')
+				return render(request, 'app/login.html')
+		except Exception as e:
+			messages.error(request, 'user dose not exist')
+			redirect('login')
+
+	return render(request, 'app/login.html')
+def registration(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		if User.objects.filter(username=username).exists():
+			messages.error(request, 'Username is already in use')
+			return render(request, 'app/registration.html')
+		password = request.POST.get('password')
+		confirm_password = request.POST.get('confirm_password')
+		if password != confirm_password:
+			messages.error(request, 'password confirmation dose not match the password !!')
+			return render(request, 'app/registration.html')
+		user = User.objects.create_user(username=username, password=password)
+		user.save()
+		auth_login(request, user)
+		request.session['username'] = username
+		request.session['creation_time'] = time.time()
+		return redirect('homepage')
+
+	return render(request, 'app/registration.html')
+
 def homepage(request):
 	current_time = time.time()
 	if 'username' not in request.session or current_time - request.session['creation_time'] > 5:
@@ -12,9 +55,3 @@ def homepage(request):
 		request.session.modified = True
 	user = request.session['username']
 	return render(request, 'app/homepage.html',{'user': user, 'time': request.session['creation_time']})
-
-# def get_name(request):
-# 	username = request.session.get('username')
-# 	# print(f"<<<<{username}>>>>")
-# 	print(f"Session Data: {request.session.items()}")
-# 	return JsonResponse({'name':username})
