8a76e6ad8b2b1cfa1259cb2bfecd2c1ae78eeff4
amindid <amine10ouchaadid@gmail.com>
2024-09-03 15:57:07 +0100

    working on auth

---
+++ srcs/WePong/WePongAuth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WepongauthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'WePongAuth'
---
+++ srcs/WePong/WePongAuth/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login(request):
+	return render(request, 'WePongAuth/login.html')
+
+def homepage(request):
+	return render(request, 'WePongAuth/homepage.html')
